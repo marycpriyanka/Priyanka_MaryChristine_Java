90e97073aba00d6ce6ab887212e9fff6670c9696
marycpriyanka <marycpriyanka@gmail.com>
2022-10-04 22:14:16 -0700

    RGPInheritance

--- M1-Challenge-MaryChristine-Priyanka/IceCreamClassDesign/src/main/java/com/company/IcecreamApplication.java
+++ M1-Challenge-MaryChristine-Priyanka/IceCreamClassDesign/src/main/java/com/company/IcecreamApplication.java
@@ -6,11 +6,7 @@ import java.util.List;
 
 public class IcecreamApplication {
     public static void main(String[] args) {
-        List<String> ingredients = new ArrayList<String>();
-        ingredients.add("cocoa powder");
-        ingredients.add("milk");
-        ingredients.add("sugar");
-        ingredients.add("chocolate chips");
+        List<String> ingredients = new ArrayList<String>(Arrays.asList("cocoa powder", "milk", "sugar", "chocolate chips"));
 
         com.company.factory.IceCream factoryIcecream = new com.company.factory.IceCream("chocolate", 10, 5, 4, ingredients);
         System.out.println("Factory Ice cream class: " + factoryIcecream.toString());
---
+++ M1-Challenge-MaryChristine-Priyanka/RGPInheritance/src/main/java/com/company/Character.java
@@ -0,0 +1,96 @@
+package com.company;
+
+public class Character {
+    private String name;
+    private int strength;
+    private int health;
+    private int stamina;
+    private int speed;
+    private int attackPower;
+    private boolean running;
+    private boolean arrested;
+
+    public Character(String name, int strength, int health, int stamina, int speed, int attackPower, boolean running, boolean arrested) {
+        this.name = name;
+        this.strength = strength;
+        this.health = health;
+        this.stamina = stamina;
+        this.speed = speed;
+        this.attackPower = attackPower;
+        this.running = running;
+        this.arrested = arrested;
+    }
+
+    /**
+     * Method to attack another character
+     * @param character - character attacked
+     */
+    public void attackAnotherCharacter(Character character) {
+        character.setStrength(character.getStrength() - this.getAttackPower());
+        this.setStrength(this.getStrength() + this.getAttackPower());
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public void setStrength(int strength) {
+        this.strength = strength;
+    }
+
+    public int getHealth() {
+        return health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getAttackPower() {
+        return attackPower;
+    }
+
+    public void setAttackPower(int attackPower) {
+        this.attackPower = attackPower;
+    }
+
+    public boolean isRunning() {
+        return running;
+    }
+
+    public void setRunning(boolean running) {
+        this.running = running;
+    }
+
+    public boolean isArrested() {
+        return arrested;
+    }
+
+    public void setArrested(boolean arrested) {
+        this.arrested = arrested;
+    }
+}
